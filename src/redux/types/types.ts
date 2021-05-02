599687ed174f757985dabdde4bbb5f7e8fdd8968
Анастасия <61273967+AnastasiyaHolubickaya@users.noreply.github.com>
2021-05-02 22:16:41 +0300

    Add files via upload

--- src/redux/types/types.ts
+++ src/redux/types/types.ts
@@ -2,3 +2,13 @@ export type CanvasPropsType ={
     width:number
     height:number
 }
+export  type coordinateType = number
+
+type contextType = CanvasRenderingContext2D | null
+
+export type findStepCollapsLineType = (mass: Array<coordinateType>, num: number) => Array<coordinateType>;
+export type lineSegmentIntersectType = (mass: Array<coordinateType>, mass2: Array<coordinateType>) => Array<coordinateType> | boolean
+export type drawType = (context:contextType, mass: Array<coordinateType>, mass2: Array<Array<coordinateType>>,massIn:Array<coordinateType>) => Array<coordinateType>
+export type drawLineType = (context:contextType, mass: Array<coordinateType>) => void
+export type drawDotType = (context:contextType, mass: Array<coordinateType>) => void
+export type lineCollapsType = (context:contextType, width:number, height:number, stepLines:Array<Array<coordinateType>>,counter:number,collaps_step:number) => void
